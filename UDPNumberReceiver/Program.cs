b29f17367ab9c6c33726d3e4630568d42600136c
Bartosz <DonMarchewa@gmail.com>
2019-11-16 18:55:05 +0100

    It gets 4 strings...
    but still need to send it to web api <-TODO

--- UDPNumberReceiver/Program.cs
+++ UDPNumberReceiver/Program.cs
@@ -24,20 +24,33 @@ namespace UDPNumberReceiver
             {
                 // Blocks until a message is received on this socket from a remote host (a client).
                 Console.WriteLine("Server is blocked");
+                float SumCO = 0;
+                float SumNOx = 0;
                 while (true)
                 {
                     Byte[] receiveBytes = udpServer.Receive(ref RemoteIpEndPoint);
                     //Server is now activated");
 
                     string receivedData = Encoding.ASCII.GetString(receiveBytes);
-                    string[] data = receivedData.Split(' ');
-                    string clientName = data[0];
-                    string text = data[1] + data[2];
+                    if (receivedData == "Stop")
+                    {
+                        Console.WriteLine("The broadcast has been stopped!");
+                        break;
+                    }
+                    else
+                    {
 
-                    Console.WriteLine(receivedData);
-                    //Console.WriteLine("Received from: " + clientName.ToString() + " " + text.ToString());
+                        string[] data = receivedData.Split(' ');
+                        string CO = data[11];
+                        string NOx = data[15];
+                        string Date = data[8];
+                        string ParticleLEvel = data[19];
+                        SumCO = SumCO + float.Parse(CO);
+                        SumNOx = SumNOx + float.Parse(NOx);
+                        Console.WriteLine(receivedData + $"\r\n Sum of all CO: {SumCO}\r\n Sum of all NOx: {SumNOx}\r\n");
+                    }
                 }
-            }
+                }
             catch (Exception e)
             {
                 Console.WriteLine(e.ToString());
